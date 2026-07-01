31e29f7a9e54f8a9d0caf2b9f0742928ec6c9000
tejasreenaram95029 <tejasree.naram21@gmail.com>
2026-07-02 01:29:10 +0530

    Assignments

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,34 @@
+console.log("line1 ")
+console.log("line2")
+console.log("line3")
+//sumOfNumbers(5,10)
+console.log("line4 ")
+console.log("line5")
+console.log("line6")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
