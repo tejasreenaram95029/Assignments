6daf3fa4aa9a07fabe8fe1c95732dc349a49d5e6
tejasree_naram <tejasree.naram21@gmail.com>
2026-07-02 11:49:43 +0530

    assignment1.ts

--- typescript-training/assignments/assignment1.ts
+++ typescript-training/assignments/assignment1.ts
@@ -1,86 +1,91 @@
 //1. Temperature of a city in degrees Celsius: 25.5
-let temp : number = 25.5;
-console.log(`1. Temperature of a city in degrees Celsius: ${temp}`)
+let temp: number = 25.5;
+//console.log(`1. Temperature of a city in degrees Celsius: ${temp}`)
+console.log("1. Temperature of a city in degrees Celsius: ",  temp);
 
 //2. Whether a customer has placed an order: true or false
-let orderPlacedTrue : boolean = true;
-let orderPlacedFalse : boolean = false;
-console.log(`2. Whether a customer has placed an order: ${orderPlacedTrue} or ${orderPlacedFalse}`)
+let orderPlacedTrue: boolean = true;
+let orderPlacedFalse: boolean = false;
+//console.log(`2. Whether a customer has placed an order: ${orderPlacedTrue} or ${orderPlacedFalse}`)
+console.log("2. Whether a customer has placed an order: ",orderPlacedTrue ," or ",  orderPlacedFalse);
 
-     
 //3. Person's phone number: "123-456-7890"
-let phoneNum : string = "123-456-7890";
-console.log(`3. Person's phone number : ${phoneNum}`)
+let phoneNum: string = "123-456-7890";
+//console.log(`3. Person's phone number : ${phoneNum}`)
+console.log("3. Person's phone number : ", phoneNum);
 
 //4. 4. Amount of money in a customer's bank account: 1000.50
-let accountBalance : number = 1000.50;
-console.log(`4. Amount of money in a customer's bank account: ${accountBalance} `)
+let accountBalance: number = 1000.50;
+//console.log(`4. Amount of money in a customer's bank account: ${accountBalance} `)
+console.log("4. Amount of money in a customer's bank account: ", accountBalance);
 
 //5. Person's email address: "john.doe@example.com"
-let emailId : string = "john.doe@example.com";
-console.log(`5. Person's email address: ${emailId}`)
+let emailId: string = "john.doe@example.com";
+//console.log(`5. Person's email address: ${emailId}`)
+console.log("5. Person's email address: ", emailId);
 
 //6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
-let latitude : number = 37.7749;
-let longitude : number = -122.4194;
-console.log(`6. Coordinates of a location (latitude, longitude): ${latitude}, ${longitude}`)
+let coordinates : number[] = [37.7749, -122.4194];
+
+//console.log(`6. Coordinates of a location (latitude, longitude): ${latitude}, ${longitude}`)
+console.log("6. Coordinates of a location (latitude, longitude):", coordinates[0], "," ,coordinates[1]);
 
 //7. Person's marital status: true or false
 let married : boolean = true;
 let unmarried : boolean = false;
-console.log(`7. Person's marital status: ${married} or ${unmarried}`)
+console.log("7. Person's marital status:", married,"or",unmarried)
 
 //8. Person's occupation: "Software Engineer"
 let occupation : string = "Software Engineer";
-console.log(`8. Person's occupation: "${occupation}"`)
+console.log("8. Person's occupation: ",occupation);
 
 //9. Person's favourite colour: "Blue"
 let favouriteColor : string = "Blue";
-console.log(`9. Person's favourite colour: "${favouriteColor}"`)
+console.log("9. Person's favourite colour: ", favouriteColor);
 
 //10.Current year: 2023
 let currentDate = new Date();
 //console.log(currentDate);
 //console.log(currentDate.getFullYear());
-console.log(`10.Current year: ${currentDate.getFullYear()}`);
+console.log("10.Current year:", currentDate.getFullYear());
 
 
 //11.Number of followers on a social media platform: 1,000,000
 let numOfFollowers : string = "1,000,000";
-console.log(`11.Number of followers on a social media platform: ${numOfFollowers}`)
+console.log("11.Number of followers on a social media platform:", numOfFollowers);
 
 //12.Rating of a movie: 7.5
 let movieRating : number = 7.5;
-console.log(`12.Rating of a movie: ${movieRating}`)
+console.log("12.Rating of a movie: ", movieRating);
 
 //13.Person's blood type: 'A'
 let bloodType : string = "A";
-console.log(`13.Person's blood type: "${bloodType}"`)
+console.log("13.Person's blood type: ", bloodType);
 
 //14.Title of a book: "To Kill a Mockingbird"
 let book : string = "To kill a Mockingbird";
-console.log(`14.Title of a book: "${book}"`)
+console.log("14.Title of a book: ", book);
 
 //15.Number of employees in a company: 500
 let numOfEmployees : number = 500;
-console.log(`15.Number of employees in a company: ${numOfEmployees}`)
+console.log("15.Number of employees in a company: ", numOfEmployees);
 
 //16.Time of an event: 2:30 PM
 let timeOfAnEvent : string = "2:30 PM";
-console.log(`16.Time of an event: ${timeOfAnEvent}`)
+console.log("16.Time of an event: ", timeOfAnEvent)
 
 //17.Name of a country: "United States"
 let nameOfACountry : string = "United States";
-console.log(`17.Name of a country: "${nameOfACountry}"`)
+console.log("17.Name of a country: ", nameOfACountry)
 
 //18.Person's eye color: "Brown"
 let eyeColor : string = "Brown";
-console.log(`18.Person's eye color: "${eyeColor}"`)
+console.log("18.Person's eye color: ", eyeColor)
 
 //19.Person's birthplace: "New York City"
 let birthPlace : string = "New York City";
-console.log(`19.Person's birthplace: "${birthPlace}"`)
+console.log("19.Person's birthplace: ", birthPlace)
 
 //20. Distance between two cities: 200.5
 let distance : number = 200.5;
-console.log(`20. Distance between two cities: ${distance}`)
+console.log("20. Distance between two cities: ", distance)
